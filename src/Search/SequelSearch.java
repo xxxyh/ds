eb79a1496665d83ac5443e9c633ff8f7ddd9aafc
xxxyh <869098783@qq.com>
2019-07-25 22:44:22 +0800

    添加顺序查找

---
+++ src/Search/SequelSearch.java
@@ -0,0 +1,20 @@
+package Search;
+
+public class SequelSearch extends BaseSearch {
+
+
+    @Override
+    public boolean search(int[] nums, int data) {
+        int n = nums.length;
+        for(int i = 0;i < n;i++){
+            if(nums[i] == data){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        new SequelSearch().doSearch(10);
+    }
+}
